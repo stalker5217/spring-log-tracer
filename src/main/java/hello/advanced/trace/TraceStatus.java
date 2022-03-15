d435f4d09a3bf4545ec6230ae6e546b5894ce676
Song <stalker5217@gmail.com>
2022-03-15 22:56:22 +0900

    first commit

---
+++ src/main/java/hello/advanced/trace/TraceStatus.java
@@ -0,0 +1,25 @@
+package hello.advanced.trace;
+
+public class TraceStatus {
+    private TraceId traceId;
+    private long startTimeMs;
+    private String message;
+
+    public TraceStatus(TraceId traceId, long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    void field() throws InterruptedException {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            threadLocalService.logic("userA");
+        };
+        Runnable userB = () -> {
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        Thread threadB = new Thread(userB);
+
+        threadA.setName("thread-A");
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+
+        threadA.join();
+        threadB.join();
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
