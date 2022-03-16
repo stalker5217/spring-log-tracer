68a1c7288e05c15e9dc1a8953da042d1b5fbebea
songmk <stalker5217@gmail.com>
2022-03-16 23:01:59 +0900

    template method pattern

---
+++ src/test/java/hello/advanced/template/TemplateMethodTest.java
@@ -0,0 +1,63 @@
+package hello.advanced.template;
+
+import org.junit.jupiter.api.Test;
+
+import hello.advanced.template.code.AbstractTemplate;
+import hello.advanced.template.code.SubClassLogic1;
+import hello.advanced.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class TemplateMethodTest {
+	@Test
+	void templateMethodV0() {
+		logic1();
+		logic2();
+	}
+
+	private void logic1() {
+		long startTime = System.currentTimeMillis(); //비즈니스 로직 실행
+		log.info("비즈니스 로직1 실행");
+		//비즈니스 로직 종료
+		long endTime = System.currentTimeMillis();
+		long resultTime = endTime - startTime;
+		log.info("resultTime={}", resultTime);
+	}
+
+	private void logic2() {
+		long startTime = System.currentTimeMillis(); //비즈니스 로직 실행
+		log.info("비즈니스 로직2 실행");
+		//비즈니스 로직 종료
+		long endTime = System.currentTimeMillis();
+		long resultTime = endTime - startTime;
+		log.info("resultTime={}", resultTime);
+	}
+
+	@Test
+	void templateMethodV1() {
+		AbstractTemplate template1 = new SubClassLogic1();
+		template1.execute();
+		AbstractTemplate template2 = new SubClassLogic2();
+		template2.execute();
+	}
+
+	@Test
+	void templateMethodV2() {
+		AbstractTemplate template1 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		};
+		log.info("클래스 이름1={}", template1.getClass());
+		template1.execute();
+		AbstractTemplate template2 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		};
+		log.info("클래스 이름2={}", template2.getClass());
+		template2.execute();
+	}
+}
