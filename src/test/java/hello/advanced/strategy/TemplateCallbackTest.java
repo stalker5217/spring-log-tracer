c2307b8106388c9e7fe73651f017dd5f3fc8e91e
songmk <stalker5217@gmail.com>
2022-03-18 21:41:55 +0900

    template callback pattern

---
+++ src/test/java/hello/advanced/strategy/TemplateCallbackTest.java
@@ -0,0 +1,36 @@
+package hello.advanced.strategy;
+
+import org.junit.jupiter.api.Test;
+
+import hello.advanced.strategy.template.Callback;
+import hello.advanced.strategy.template.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class TemplateCallbackTest {
+	@Test
+	void callbackV1() {
+		TimeLogTemplate template = new TimeLogTemplate();
+
+		template.execute(new Callback() {
+			@Override
+			public void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		});
+
+		template.execute(new Callback() {
+			@Override
+			public void call() {
+				log.info("비즈니스 로직2 실행");
+			}
+		});
+	}
+
+	@Test
+	void callbackV2() {
+		TimeLogTemplate template = new TimeLogTemplate();
+		template.execute(() -> log.info("비즈니스 로직1 실행"));
+		template.execute(() -> log.info("비즈니스 로직2 실행"));
+	}
+}
